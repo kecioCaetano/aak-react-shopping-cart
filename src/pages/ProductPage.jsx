eedd6fc2ff530bedc48c2dde403f8c01d88ae637
Kecio Caetano Barbosa <kecio.kecio@gmail.com>
2026-06-17 06:46:58 -0400

    product page featured

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -70,15 +70,109 @@ function ProductPage() {
         <>
             <main className="pd-pg">
                 <section className="pd-pg__img--container">
-                    <nav to="/" className="pd-pg__nav">HOME
-                        <Link>{' '}/
-
-                        </Link>
+                    <nav className="pd-pg__nav">
+                        <Link to="/" className="pd-pg__nav-link">HOME</Link>
+                        {' '}/
+                        {
+                            product.type === 'sunnies'
+                                ? (<Link to='/products/sunglasses' className="pd-pg__nav-link">Sunglasses</Link>)
+                                : (<Link to='/products/eyeglasses' className="pd-pg__nav-link">Eyeglasses</Link>)
+                        }{' '}/
+                        <span className="pd-pg__nav-link">{product.brand}</span>
                     </nav>
-                    <div></div>
+                    <div className="pd-pg__img--wrapper">
+                        <ImageWrapper>
+                            {
+                                activeImg === 1
+                                    ? (<img src={product.img.main} alt={`${product.tile} front view`} className="pd-pg__active-img" />)
+                                    : (<img src={product.images.side} alt={`${product.title} side view`} className="pd-pg__active-img" />)
+                            }
+                        </ImageWrapper>
+                        <div className="pd-pg__img-thumbs">
+                            <button
+                                className={`pd-pg__img-thumbnail--wrapper ${activeImg === 1 ? 'active' : ''
+                                    }`}
+                                type="button"
+                                onClick={() => setActiveImg(1)}
+                            >
+                                <img
+                                    src={product.images.main}
+                                    alt={`${product.title} front view`}
+                                    className="pd-pg__img-thumbnail"
+                                />
+                            </button>
+                            <button
+                                className={`pd-pg__img-thumbnail--wrapper ${activeImg === 2 ? 'active' : ''
+                                    }`}
+                                type="button"
+                                onClick={() => setActiveImg(2)}
+                            >
+                                <img
+                                    src={product.images.side}
+                                    alt={`${product.title} side view`}
+                                    className="pd-pg__img-thumbnail"
+                                />
+                            </button>
+
+                        </div>
+                    </div>
+                </section >
+                <section className="pd-pg__details--container">
+                    <h1 className="pd-pg__title">{product.title}</h1>
+                    <div className="pd-pg__price--wrapper">
+                        <h3 className="pd-pg__price">{product.price}</h3>
+                        {product.onSale && (
+                            <h3 className="pd-pg__og-price">{product.originalPrice}</h3>
+                        )}
+                    </div>
+                    <div className="pd-pg__details">
+                        <p className="pd-pg__description">{product.description}</p>
+                        <ul className="pd-pg__tech-details">
+                            {product.technicalDetails.map((td) => (
+                                <li className="pd-pg__tech-detail" key={td}>{td}</li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="pd-pg__opt-details">
+                        {product.color && (
+                            <div className="pd-pg__color--wrapper">
+                                <span className=" pd-pg__opt-label pd-pg__color-label">
+                                    Color:{' '}
+                                </span>
+                                <span className="pd-pg__color">{product.color}</span>
+                            </div>
+                        )}
+                        {product.quantity > 0 ? (
+                            <form onSubmit={addToCart} noValidate>
+                                <label htmlFor="pd-quantity" className="pd-pg__opt-label pd-pg__quantity-label">
+                                    Quantity:
+                                    <div className="pd-pg__quantity-wrapper">
+                                        <button type='button' className="pd-pg__quantity-btn" onClick={decrement} disabled={desiredQty <= 1}>-</button>
+                                        <input type="number" className="pd-pg__quantity-input" name="pd-quantity" id="pd-quantity"
+                                            max={product.quantity} min={1} onChange={handleDesiredQty} value={desiredQty} />
+                                        <button type="button" className="pd-pg__quantity-btn" disabled={desiredQty >= product.quantity} onClick={increment} >+</button>
+                                    </div>
+                                </label>
+                                <button className="pd-pg__btn-card pd-pg__btn-add-cart" type="submit" >Add to Cart</button>
+                                {itemInCart && (
+                                    <motion.div initial={{ scaleY: 0, opacity: 0 }}
+                                        animate={{ scaleY: 1, originY: 0, opacity: 1 }}
+                                        transition={{ duration: 0.5 }}>
+                                        <Link to="/cart" className="pd-pg__btn-cart pd-pg__btn-checkout">Buy it now</Link>
+                                    </motion.div>
+                                )}
+                                {errorMessage && (
+                                    <span className="pd-pg__error">{errorMEssage}</span>
+                                )}
+                            </form>
+                        ) : (
+                            <div className="pd-pg__quantity-sold-out">
+                                Item is currently sold out. Keep posted for updates!
+                            </div>
+                        )}
+                    </div>
                 </section>
-                <section className="pd-pg__details--container">a</section>
-            </main>
+            </main >
             <section>b</section>
         </>
     )
